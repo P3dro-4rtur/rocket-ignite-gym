0f74df346c2351ec86eadb754cb3ace6b8e7437d
Pedro Artur Wingert <pedro.awingert@gmail.com>
2023-02-09 13:46:35 -0300

    feat: add tema app

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { StatusBar } from "react-native";
 import { NativeBaseProvider } from "native-base";
 import { Loader } from "@components/Loader";
-
+import { THEME } from "~/theme";
 import {
   useFonts,
   Roboto_400Regular,
@@ -13,7 +13,7 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         translucent
         barStyle={"light-content"}
